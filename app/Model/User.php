f8b3048f496eb31ad0cd9a8b1c851b1e7f0105ac
zsfirefly <zsfirefly@qq.com>
2021-10-15 16:47:34 +0800

    修改正确的名称

--- app/Model/User.php
+++ app/Model/User.php
@@ -11,7 +11,7 @@ class User extends Model
 	  
 	protected $fillable = ['nickname','avatar','phone','code','desc','passed','openid'];
 	
-	public function accouts()
+	public function accounts()
     {
          return $this->hasMany('App\Model\Account');
     }
