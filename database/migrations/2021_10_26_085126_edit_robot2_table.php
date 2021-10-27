d1b846e3ae19f0afcc83f846a2d7dca8caad2fa2
zsfirefly <zsfirefly@qq.com>
2021-10-27 17:28:38 +0800

    000

--- database/migrations/2021_10_26_085126_edit_robot2_table.php
+++ database/migrations/2021_10_26_085126_edit_robot2_table.php
@@ -14,10 +14,7 @@ class EditRobot2Table extends Migration
     public function up()
     {
         //
-		Schema::table('machines', function (Blueprint $table) {
-			$table->integer('move_speed')->nullable();
-			$table->integer('power_setting')->nullable();
-		});
+
 		
 		Schema::create('users', function (Blueprint $table) {
 			$table->tinyInteger('is_first')->default(0)->comment('是否第一次登录为1,默认为0');
