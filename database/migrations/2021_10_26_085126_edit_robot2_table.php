44c42a1a59d75635dacde34edf84a10457aa4065
zsfirefly <zsfirefly@qq.com>
2021-10-27 17:07:52 +0800

    修改

--- database/migrations/2021_10_26_085126_edit_robot2_table.php
+++ database/migrations/2021_10_26_085126_edit_robot2_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class EditRobot1Table extends Migration
+class EditRobot2Table extends Migration
 {
     /**
      * Run the migrations.
