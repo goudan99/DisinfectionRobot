67feb44480c538ca0e45ff9f504fb82edb7f747b
zsfirefly <zsfirefly@qq.com>
2021-10-27 16:57:04 +0800

    添加数据库字段 user is_first是否第一次登录 machine  move_speed 移动速度  power_setting 充电设置

---
+++ database/migrations/2021_10_26_085126_edit_robot2_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditRobot1Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::table('machines', function (Blueprint $table) {
+			$table->integer('move_speed')->nullable();
+			$table->integer('power_setting')->nullable();
+		});
+		
+		Schema::create('users', function (Blueprint $table) {
+			$table->tinyInteger('is_first')->default(0)->comment('是否第一次登录为1,默认为0');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
