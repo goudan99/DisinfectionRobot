2cbe2964969998ddf207fe5a92e4039f35904f3b
zsfirefly <zsfirefly@qq.com>
2021-11-09 10:33:06 +0800

    还原

--- app/Model/Job.php
+++ app/Model/Job.php
@@ -13,19 +13,4 @@ class Job extends Model
         'map_area' => 'collection',
         'work' => 'collection'
     ];
-	
-    public function machine()
-    {
-        return $this->belongsToMany('App\Model\Machine');
-    }
-
-    public function map()
-    {
-        return $this->belongsToMany('App\Model\Map');
-    }
-	
-    public function user()
-    {
-        return $this->belongsToMany('App\Model\User');
-    }	
 }
