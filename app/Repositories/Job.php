88b8374b08f6e2e72326003501a03ed41fb061d5
zsfirefly <zsfirefly@qq.com>
2021-11-10 19:17:34 +0800

    修改

--- app/Repositories/Job.php
+++ app/Repositories/Job.php
@@ -49,7 +49,7 @@ class Job implements Repository
         if(isset($data["machine_id"])){
 			
 			if($machine=Machine::where('id',$data["machine_id"])->first()){$data['machine_name']=$machine->sn;}
-
+			$user=$notify["form"]["user"];
 			if(!($user->id==1||$user->roles()->where('level',1)->first())){
 				if(!$notify["form"]["user"]->machines()->where("id",$data["machine_id"])->first()){
 					throw ValidationException::withMessages(["machine_id" => "该机器不存在，或者你没有权限操控此机器"]);
